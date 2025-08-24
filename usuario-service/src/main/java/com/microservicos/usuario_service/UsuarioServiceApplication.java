c5a26e7947193743f49e37ff21d9a8c3cb1c8ddf
oscardiaz28 <diazvargasod@gmail.com>
2025-08-24 10:01:05 -0500

    eureka

--- usuario-service/src/main/java/com/microservicos/usuario_service/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/microservicos/usuario_service/UsuarioServiceApplication.java
@@ -2,11 +2,13 @@ package com.microservicos.usuario_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 // anotacion para habilitar al cliente Feign
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UsuarioServiceApplication {
 
 	public static void main(String[] args) {
